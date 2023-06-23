07fb6f3b773df88a4269c66dd67e2ca5ada1996d
naripok <fernando.canteruccio@gmail.com>
2023-06-23 10:15:24 -0300

    fix: product section flex flow and shadow-light

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        light:
+          "0 4px 8px 0 rgba(0, 0, 0, 0.08), 0 8px 30px 0 rgba(0, 0, 0, 0.03)",
+      },
+    },
   },
   plugins: [],
 };
