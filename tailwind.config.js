821799a32f3ba4d4c0f1e3951eb6dad7b4e359ba
naripok <fernando.canteruccio@gmail.com>
2023-06-23 15:01:25 -0300

    feat: WIP testimonials carousel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const plugin = require("tailwindcss/plugin");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html"],
@@ -9,5 +11,83 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addBase, addUtilities, theme }) {
+      addBase({
+        body: {
+          "-webkit-font-smoothing": "subpixel-antialiased",
+        },
+      });
+
+      /**
+       * Typography (with OpenType) Utilities
+       */
+
+      const typographyUtils = {
+        ".optimize-legibility": {
+          "text-rendering": "optimizeLegibility",
+        },
+        ".ligatures": {
+          "font-variant-ligatures": "common-ligatures",
+          "font-feature-settings": "'liga' on, 'clig' on",
+        },
+        ".kerning": {
+          "font-kerning": "normal",
+          "font-feature-settings": "'kern' on",
+        },
+        ".small-caps": {
+          "font-variant-caps": "small-caps",
+          "font-feature-settings": "'smcp' on",
+        },
+        ".all-small-caps": {
+          "font-variant-caps": "all-small-caps",
+          "font-feature-settings": "'c2sc' on, 'smcp' on",
+        },
+        ".lining-nums": {
+          "font-variant-numeric": "lining-nums",
+          "font-feature-settings": "'lnum' on",
+        },
+        ".oldstyle-nums": {
+          "font-variant-numeric": "oldstyle-nums",
+          "font-feature-settings": "'onum' on",
+        },
+        ".proportional-nums": {
+          "font-variant-numeric": "proportional-nums",
+          "font-feature-settings": "'pnum' on",
+        },
+        ".tabular-nums": {
+          "font-variant-numeric": "tabular-nums",
+          "font-feature-settings": "'tnum' on",
+        },
+        ".slashed-zero": {
+          "font-variant-numeric": "slashed-zero",
+          "font-feature-settings": "'zero' on",
+        },
+        ".super": {
+          "font-variant-position": "super",
+          "font-feature-settings": "'sups' on",
+        },
+        ".sub": {
+          "font-variant-position": "sub",
+          "font-feature-settings": "'sub' on",
+        },
+        ".ordinal": {
+          "font-variant-position": "ordinal",
+          "font-feature-settings": "'ordn' on",
+        },
+        ".fractions": {
+          "font-variant-numeric": "diagonal-fractions",
+          "font-feature-settings": "'frac' on",
+        },
+        ".case-sensitive": {
+          "text-transform": "uppercase",
+          "font-feature-settings": "'case' on",
+        },
+      };
+
+      addUtilities(typographyUtils, {
+        variants: ["responsive"],
+      });
+    }),
+  ],
 };
