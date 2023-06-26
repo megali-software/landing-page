0b116aa104de1f08febc2252dce43126ef793a0c
naripok <fernando.canteruccio@gmail.com>
2023-06-26 09:08:03 -0300

    chore: lint

--- src/js/typewriter.js
+++ src/js/typewriter.js
@@ -1,5 +1,5 @@
 let i = 0;
-let txt = ['Partners', 'Professionals', 'Companies']; // The text
+let txt = ["Partners", "Professionals", "Companies"]; // The text
 let currentText = 0;
 let isDeleting = false;
 let speed = 30; // Speed of typing in milliseconds
@@ -10,23 +10,34 @@ function typeWriter() {
   let displayText = txt[currentText];
 
   if (isDeleting) {
-    document.getElementById("typewriter").innerHTML = displayText.substring(0, i) + '<span class="blinking-cursor">|</span>';
+    document.getElementById("typewriter").innerHTML =
+      displayText.substring(0, i) + '<span class="blinking-cursor">|</span>';
     if (i === 0) {
       isDeleting = false;
       currentText = (currentText + 1) % txt.length;
-      setTimeout(typeWriter, speed + Math.floor(Math.random() * randomSpeedVariation));
+      setTimeout(
+        typeWriter,
+        speed + Math.floor(Math.random() * randomSpeedVariation)
+      );
     } else {
       i--;
-      setTimeout(typeWriter, speed + Math.floor(Math.random() * randomSpeedVariation));
+      setTimeout(
+        typeWriter,
+        speed + Math.floor(Math.random() * randomSpeedVariation)
+      );
     }
   } else {
-    document.getElementById("typewriter").innerHTML = displayText.substring(0, i) + '<span class="blinking-cursor">|</span>';
+    document.getElementById("typewriter").innerHTML =
+      displayText.substring(0, i) + '<span class="blinking-cursor">|</span>';
     if (i === displayText.length) {
       isDeleting = true;
       setTimeout(typeWriter, delayBetweenWords); // Only delay after a full word has been typed
     } else {
       i++;
-      setTimeout(typeWriter, speed + Math.floor(Math.random() * randomSpeedVariation));
+      setTimeout(
+        typeWriter,
+        speed + Math.floor(Math.random() * randomSpeedVariation)
+      );
     }
   }
 }
