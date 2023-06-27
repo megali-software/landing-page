e68ef53d5012ed81d4fedf47763cc490ee5b42a3
naripok <fernando.canteruccio@gmail.com>
2023-06-27 09:56:32 -0300

    feat: optimize typeWriter

--- src/js/typewriter.js
+++ src/js/typewriter.js
@@ -1,43 +1,33 @@
-let i = 0;
-let txt = ["Partners", "Professionals", "Companies"]; // The text
+const txt = ["Partners", "Professionals", "Companies"];
+const speed = 50;
+const delayBetweenWords = 2300;
+
+let i = txt[0].length;
 let currentText = 0;
-let isDeleting = false;
-let speed = 30; // Speed of typing in milliseconds
-let delayBetweenWords = 2300; // Delay between words in milliseconds
-let randomSpeedVariation = 20; // Variation in typing speed in milliseconds
+let isDeleting = true;
 
 function typeWriter() {
   let displayText = txt[currentText];
 
+  document.getElementById("typewriter").innerHTML =
+    displayText.substring(0, i) + '<span class="blinking-cursor">|</span>';
+
   if (isDeleting) {
-    document.getElementById("typewriter").innerHTML =
-      displayText.substring(0, i) + '<span class="blinking-cursor">|</span>';
     if (i === 0) {
       isDeleting = false;
       currentText = (currentText + 1) % txt.length;
-      setTimeout(
-        typeWriter,
-        speed + Math.floor(Math.random() * randomSpeedVariation)
-      );
+      setTimeout(typeWriter, speed);
     } else {
       i--;
-      setTimeout(
-        typeWriter,
-        speed + Math.floor(Math.random() * randomSpeedVariation)
-      );
+      setTimeout(typeWriter, speed);
     }
   } else {
-    document.getElementById("typewriter").innerHTML =
-      displayText.substring(0, i) + '<span class="blinking-cursor">|</span>';
     if (i === displayText.length) {
       isDeleting = true;
       setTimeout(typeWriter, delayBetweenWords); // Only delay after a full word has been typed
     } else {
       i++;
-      setTimeout(
-        typeWriter,
-        speed + Math.floor(Math.random() * randomSpeedVariation)
-      );
+      setTimeout(typeWriter, speed);
     }
   }
 }
